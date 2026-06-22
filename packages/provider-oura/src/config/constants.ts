1b00bb969b3297293c1b74e7de709d2a37ee13dc
BerkYildizgorer <berk.yildizgorer@vuse-xr.com>
2026-06-22 15:02:47 +0200

    initial package structure for the oura ring connector

---
+++ packages/provider-oura/src/config/constants.ts
@@ -0,0 +1,12 @@
+const CLIENT_ID = "YOUR_CLIENT_ID";
+const REDIRECT_URI = "YOUR_REDIRECT_URI";
+const SUPPORTED_SCOPES = [
+  "daily",
+  "heartrate",
+  "hrv",
+  "activity",
+  "sleep",
+  "readiness",
+];
+
+export { CLIENT_ID, REDIRECT_URI, SUPPORTED_SCOPES };
---
+++ packages/provider-oura/src/index.ts
@@ -0,0 +1,6 @@
+export interface OuraConnectorParams {
+  accessToken: string;
+  patientId: string;
+  startDate: string;
+  endDate: string;
+}
