d0a2f579e8a0ce14255c63ce19664637e2e50772
BerkYildizgorer <berk.yildizgorer@vuse-xr.com>
2026-07-01 13:37:48 +0200

    constant client id secret and redirect uri is now pulled from env file. Added an example env file. Added getAccessToken and refreshAccessToken functions to index file.

--- packages/provider-oura/src/api/client.ts
+++ packages/provider-oura/src/api/client.ts
@@ -1,28 +1,40 @@
+import { CLIENT_ID, REDIRECT_URI } from '../config/constants';
 import { getOuraOauthTokenUrl } from './endpoints';
-import { type TokenRequest, type TokenResponse, TokenResponseSchema } from './schemas/auth';
+import { type TokenResponse, TokenResponseSchema } from './schemas/auth';
 
-export async function getAccessToken(request: TokenRequest): Promise<TokenResponse> {
-  const token_url = getOuraOauthTokenUrl();
-  const response = await fetch(token_url, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    body: new URLSearchParams(request).toString()
-  }).then((res) => res.json());
+export async function getAccessToken(code: string): Promise<TokenResponse> {
+  if (!CLIENT_ID || !REDIRECT_URI) {
+    throw new Error('Missing required OAuth parameters (client_id, or redirect_uri).');
+  }
 
-  return TokenResponseSchema.parse(response);
-}
-
-export async function refreshAccessToken(client_id: string, refresh_token: string): Promise<TokenResponse> {
-  const token_url = getOuraOauthTokenUrl();
-  const response = await fetch(token_url, {
+  const response = await fetch(getOuraOauthTokenUrl(), {
     method: 'POST',
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded'
     },
     body: new URLSearchParams({
-      client_id,
+      client_id: CLIENT_ID,
+      grant_type: 'authorization_code',
+      code: code,
+      redirect_uri: REDIRECT_URI
+    }).toString()
+  }).then((res) => res.json());
+
+  return TokenResponseSchema.parse(response);
+}
+
+export async function refreshAccessToken(refresh_token: string): Promise<TokenResponse> {
+  if (!CLIENT_ID) {
+    throw new Error('Missing required OAuth parameter: client_id.');
+  }
+
+  const response = await fetch(getOuraOauthTokenUrl(), {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: new URLSearchParams({
+      client_id: CLIENT_ID,
       grant_type: 'refresh_token',
       refresh_token
     }).toString()
